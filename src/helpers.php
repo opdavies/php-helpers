3b96de3da1e60dccaf32247bd0319fab4f4ac231
Oliver Davies <oliver@oliverdavies.uk>
2019-06-29 13:18:14 +0100

    Add dd function

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,13 @@
 <?php
 
+if (!function_exists('dd')) {
+    function dd(): void
+    {
+        dump(func_get_args());
+        die;
+    }
+}
+
 if (!function_exists('env')) {
     function env(string $name, ?string $default = null): ?string
     {
